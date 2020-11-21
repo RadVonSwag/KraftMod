1e5408970239d18b3c8bbd058e5c2f71fcd36adc
Defin <Defin@THE-BEAST>
2020-11-20 23:33:48 -0600

    Cheese Stick and Pickaxe

---
+++ src/main/java/net/mcreator/kraftsingles/item/ItemCheeseStonePickaxe.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.kraftsingles.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemPickaxe;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+
+import net.mcreator.kraftsingles.ElementsKraftSingles;
+
+import java.util.Set;
+import java.util.HashMap;
+
+@ElementsKraftSingles.ModElement.Tag
+public class ItemCheeseStonePickaxe extends ElementsKraftSingles.ModElement {
+	@GameRegistry.ObjectHolder("kraftsingles:cheesestonepickaxe")
+	public static final Item block = null;
+	public ItemCheeseStonePickaxe(ElementsKraftSingles instance) {
+		super(instance, 53);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemPickaxe(EnumHelper.addToolMaterial("CHEESESTONEPICKAXE", 2, 94, 5f, 2f, 9)) {
+			{
+				this.attackSpeed = -3f;
+			}
+			public Set<String> getToolClasses(ItemStack stack) {
+				HashMap<String, Integer> ret = new HashMap<String, Integer>();
+				ret.put("pickaxe", 2);
+				return ret.keySet();
+			}
+		}.setUnlocalizedName("cheesestonepickaxe").setRegistryName("cheesestonepickaxe").setCreativeTab(CreativeTabs.TOOLS));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("kraftsingles:cheesestonepickaxe", "inventory"));
+	}
+}
