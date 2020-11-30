56d6bf25ffe1450ea8cff60e1bd2d97d549a2c02
Defin <Defin@THE-BEAST>
2020-11-30 16:43:11 -0600

    Nigers

--- src/main/java/net/mcreator/kraftsingles/procedure/ProcedureCumming.java
+++ src/main/java/net/mcreator/kraftsingles/procedure/ProcedureCumming.java
@@ -6,15 +6,23 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.common.MinecraftForge;
 
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.Entity;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
 
 import net.mcreator.kraftsingles.item.ItemNujabes;
 import net.mcreator.kraftsingles.item.ItemEmptyJar;
 import net.mcreator.kraftsingles.item.ItemCumJar;
 import net.mcreator.kraftsingles.ElementsKraftSingles;
+
+import java.util.Iterator;
 
 @ElementsKraftSingles.ModElement.Tag
 public class ProcedureCumming extends ElementsKraftSingles.ModElement {
@@ -40,6 +48,25 @@ public class ProcedureCumming extends ElementsKraftSingles.ModElement {
 					ItemStack _setstack = new ItemStack(ItemCumJar.block, (int) (1));
 					_setstack.setCount(1);
 					ItemHandlerHelper.giveItemToPlayer(((EntityPlayer) entity), _setstack);
+				}
+				if (((((entity instanceof EntityPlayerMP) && (entity.world instanceof WorldServer))
+						? ((EntityPlayerMP) entity).getAdvancements()
+								.getProgress(((WorldServer) entity.world).getAdvancementManager()
+										.getAdvancement(new ResourceLocation("kraftsingles:listentonujabesandjackoff")))
+								.isDone()
+						: false) == (false))) {
+					if (entity instanceof EntityPlayerMP) {
+						Advancement _adv = ((MinecraftServer) ((EntityPlayerMP) entity).mcServer).getAdvancementManager()
+								.getAdvancement(new ResourceLocation("kraftsingles:listentonujabesandjackoff"));
+						AdvancementProgress _ap = ((EntityPlayerMP) entity).getAdvancements().getProgress(_adv);
+						if (!_ap.isDone()) {
+							Iterator _iterator = _ap.getRemaningCriteria().iterator();
+							while (_iterator.hasNext()) {
+								String _criterion = (String) _iterator.next();
+								((EntityPlayerMP) entity).getAdvancements().grantCriterion(_adv, _criterion);
+							}
+						}
+					}
 				}
 			}
 		}
